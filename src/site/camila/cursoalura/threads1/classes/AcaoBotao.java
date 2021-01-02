82197a353fe6afc0920520f34e694ff133eac48a
cami <camiladsantoscavalcante@gmail.com>
2021-01-02 07:07:24 -0300

    util class 01.06 - Criando a primeira Thread

---
+++ src/site/camila/cursoalura/threads1/classes/AcaoBotao.java
@@ -0,0 +1,27 @@
+package site.camila.cursoalura.threads1.classes;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class AcaoBotao implements ActionListener {
+
+    private JTextField primeiro;
+    private JTextField segundo;
+    private JLabel resultado;
+
+    public AcaoBotao(JTextField primeiro, JTextField segundo, JLabel resultado) {
+        this.primeiro = primeiro;
+        this.segundo = segundo;
+        this.resultado = resultado;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    	Runnable tarefa = new TarefaMultiplicacao(primeiro, segundo, resultado);
+    	Thread threadCalculo = new Thread(tarefa, "Thread calculador");
+    	threadCalculo.start();
+    }
+}
---
+++ src/site/camila/cursoalura/threads1/executavel/Principal.java
@@ -0,0 +1,10 @@
+package site.camila.cursoalura.threads1.executavel;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
